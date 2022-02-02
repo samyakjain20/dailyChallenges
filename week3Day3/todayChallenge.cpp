222027db258a8eb3ca5941887a587fa8b780d4a1
Samyak Jain <samyakjain971@gmail.com>
2022-02-02 20:57:34 +0530

    week3-day3

---
+++ week3Day3/todayChallenge.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int avgWaitTime(int orderT[], int cookT[], int n){
+    int currT=0;                            //to maintain time 
+    int waitT=0;                            //adding the wait time
+    int ctr=0, minT = INT_MAX, id=0;
+    bool flag= false, orders[n];
+    memset(orders, true, sizeof(orders));   //in start all orders need to be cooked
+    
+    while(ctr<n){
+        for(int i=0; i<n; i++)
+            if(orders[i] && orderT[i] <= currT && minT >= cookT[i]){
+                flag = true;
+                minT = cookT[i];            //min cooking time
+                id = i;                     //index of the order with min cooking time
+            }
+        
+        if(flag){
+            flag = false;
+            orders[id] = false;             //order is cooked
+            minT = INT_MAX;
+            currT += cookT[id];             //time after this order is cooked
+            waitT += (currT - orderT[id]);  //adding the wait for this order
+            ctr++;                          //increase the count for order's served 
+        }else currT++;
+    }
+    
+    return waitT/n;
+} 
+
+int main() {
+    int n;
+    cin>>n;
+    
+    int orderT[n], cookT[n];
+    for(int i=0; i<n; i++)
+        cin>>orderT[i]>>cookT[i];
+        
+    cout<<"Avg Wait Time will be: "<<avgWaitTime(orderT, cookT, n);
+	return 0;
+}
